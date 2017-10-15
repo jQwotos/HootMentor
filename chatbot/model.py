77873a15a49107ef0c0e55979724b2873e9507f1
Jason Le <le.kent.jason@gmail.com>
2017-10-15 02:52:47 -0400

    add: upload chatbot

--- chatbot/model.py
+++ chatbot/model.py
@@ -1,130 +1,244 @@
-""" A neural chatbot using sequence to sequence model with
-attentional decoder. 
-This is based on Google Translate Tensorflow model 
-https://github.com/tensorflow/models/blob/master/tutorials/rnn/translate/
-Sequence to sequence model by Cho et al.(2014)
-Created by Chip Huyen as the starter code for assignment 3,
-class CS 20SI: "TensorFlow for Deep Learning Research"
-cs20si.stanford.edu
-This file contains the code to build the model
-See readme.md for instruction on how to run the starter code.
+# Copyright 2015 Conchylicultor. All Rights Reserved.
+# Modifications copyright (C) 2016 Carlos Segura
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
 """
-from __future__ import print_function
+Model to predict the next sentence given an input sequence
+"""
 
-import time
-
-import numpy as np
 import tensorflow as tf
 
-import config
+from chatbot.textdata import Batch
 
-class ChatBotModel(object):
-    def __init__(self, forward_only, batch_size):
-        """forward_only: if set, we do not construct the backward pass in the model.
+
+class ProjectionOp:
+    """ Single layer perceptron
+    Project input tensor on the output dimension
+    """
+    def __init__(self, shape, scope=None, dtype=None):
         """
-        print('Initialize new model')
-        self.fw_only = forward_only
-        self.batch_size = batch_size
-    
-    def _create_placeholders(self):
-        # Feeds for inputs. It's a list of placeholders
-        print('Create placeholders')
-        self.encoder_inputs = [tf.placeholder(tf.int32, shape=[None], name='encoder{}'.format(i))
-                               for i in range(config.BUCKETS[-1][0])]
-        self.decoder_inputs = [tf.placeholder(tf.int32, shape=[None], name='decoder{}'.format(i))
-                               for i in range(config.BUCKETS[-1][1] + 1)]
-        self.decoder_masks = [tf.placeholder(tf.float32, shape=[None], name='mask{}'.format(i))
-                              for i in range(config.BUCKETS[-1][1] + 1)]
+        Args:
+            shape: a tuple (input dim, output dim)
+            scope (str): encapsulate variables
+            dtype: the weights type
+        """
+        assert len(shape) == 2
 
-        # Our targets are decoder inputs shifted by one (to ignore <s> symbol)
-        self.targets = self.decoder_inputs[1:]
-        
-    def _inference(self):
-        print('Create inference')
-        # If we use sampled softmax, we need an output projection.
+        self.scope = scope
+
+        # Projection on the keyboard
+        with tf.variable_scope('weights_' + self.scope):
+            self.W_t = tf.get_variable(
+                'weights',
+                shape,
+                # initializer=tf.truncated_normal_initializer()  # TODO: Tune value (fct of input size: 1/sqrt(input_dim))
+                dtype=dtype
+            )
+            self.b = tf.get_variable(
+                'bias',
+                shape[0],
+                initializer=tf.constant_initializer(),
+                dtype=dtype
+            )
+            self.W = tf.transpose(self.W_t)
+
+    def getWeights(self):
+        """ Convenience method for some tf arguments
+        """
+        return self.W, self.b
+
+    def __call__(self, X):
+        """ Project the output of the decoder into the vocabulary space
+        Args:
+            X (tf.Tensor): input value
+        """
+        with tf.name_scope(self.scope):
+            return tf.matmul(X, self.W) + self.b
+
+
+class Model:
+    """
+    Implementation of a seq2seq model.
+    Architecture:
+        Encoder/decoder
+        2 LTSM layers
+    """
+
+    def __init__(self, args, textData):
+        """
+        Args:
+            args: parameters of the model
+            textData: the dataset object
+        """
+        print("Model creation...")
+
+        self.textData = textData  # Keep a reference on the dataset
+        self.args = args  # Keep track of the parameters of the model
+        self.dtype = tf.float32
+
+        # Placeholders
+        self.encoderInputs  = None
+        self.decoderInputs  = None  # Same that decoderTarget plus the <go>
+        self.decoderTargets = None
+        self.decoderWeights = None  # Adjust the learning to the target sentence size
+
+        # Main operators
+        self.lossFct = None
+        self.optOp = None
+        self.outputs = None  # Outputs of the network, list of probability for each words
+
+        # Construct the graphs
+        self.buildNetwork()
+
+    def buildNetwork(self):
+        """ Create the computational graph
+        """
+
+        # TODO: Create name_scopes (for better graph visualisation)
+        # TODO: Use buckets (better perfs)
+
+        # Parameters of sampled softmax (needed for attention mechanism and a large vocabulary size)
+        outputProjection = None
         # Sampled softmax only makes sense if we sample less than vocabulary size.
-        if config.NUM_SAMPLES > 0 and config.NUM_SAMPLES < config.DEC_VOCAB:
-            w = tf.get_variable('proj_w', [config.HIDDEN_SIZE, config.DEC_VOCAB])
-            b = tf.get_variable('proj_b', [config.DEC_VOCAB])
-            self.output_projection = (w, b)
+        if 0 < self.args.softmaxSamples < self.textData.getVocabularySize():
+            outputProjection = ProjectionOp(
+                (self.textData.getVocabularySize(), self.args.hiddenSize),
+                scope='softmax_projection',
+                dtype=self.dtype
+            )
 
-        def sampled_loss(inputs, labels):
-            labels = tf.reshape(labels, [-1, 1])
-            return tf.nn.sampled_softmax_loss(tf.transpose(w), b, inputs, labels, 
-                                              config.NUM_SAMPLES, config.DEC_VOCAB)
-        self.softmax_loss_function = sampled_loss
+            def sampledSoftmax(labels, inputs):
+                labels = tf.reshape(labels, [-1, 1])  # Add one dimension (nb of true classes, here 1)
 
-        single_cell = tf.nn.rnn_cell.GRUCell(config.HIDDEN_SIZE)
-        self.cell = tf.nn.rnn_cell.MultiRNNCell([single_cell] * config.NUM_LAYERS)
+                # We need to compute the sampled_softmax_loss using 32bit floats to
+                # avoid numerical instabilities.
+                localWt     = tf.cast(outputProjection.W_t,             tf.float32)
+                localB      = tf.cast(outputProjection.b,               tf.float32)
+                localInputs = tf.cast(inputs,                           tf.float32)
 
-    def _create_loss(self):
-        print('Creating loss... \nIt might take a couple of minutes depending on how many buckets you have.')
-        start = time.time()
-        def _seq2seq_f(encoder_inputs, decoder_inputs, do_decode):
-            return tf.nn.seq2seq.embedding_attention_seq2seq(
-                    encoder_inputs, decoder_inputs, self.cell,
-                    num_encoder_symbols=config.ENC_VOCAB,
-                    num_decoder_symbols=config.DEC_VOCAB,
-                    embedding_size=config.HIDDEN_SIZE,
-                    output_projection=self.output_projection,
-                    feed_previous=do_decode)
+                return tf.cast(
+                    tf.nn.sampled_softmax_loss(
+                        localWt,  # Should have shape [num_classes, dim]
+                        localB,
+                        labels,
+                        localInputs,
+                        self.args.softmaxSamples,  # The number of classes to randomly sample per batch
+                        self.textData.getVocabularySize()),  # The number of classes
+                    self.dtype)
 
-        if self.fw_only:
-            self.outputs, self.losses = tf.nn.seq2seq.model_with_buckets(
-                                        self.encoder_inputs, 
-                                        self.decoder_inputs, 
-                                        self.targets,
-                                        self.decoder_masks, 
-                                        config.BUCKETS, 
-                                        lambda x, y: _seq2seq_f(x, y, True),
-                                        softmax_loss_function=self.softmax_loss_function)
-            # If we use output projection, we need to project outputs for decoding.
-            if self.output_projection:
-                for bucket in range(len(config.BUCKETS)):
-                    self.outputs[bucket] = [tf.matmul(output, 
-                                            self.output_projection[0]) + self.output_projection[1]
-                                            for output in self.outputs[bucket]]
+        # Creation of the rnn cell
+        def create_rnn_cell():
+            encoDecoCell = tf.contrib.rnn.BasicLSTMCell(  # Or GRUCell, LSTMCell(args.hiddenSize)
+                self.args.hiddenSize,
+            )
+            if not self.args.test:  # TODO: Should use a placeholder instead
+                encoDecoCell = tf.contrib.rnn.DropoutWrapper(
+                    encoDecoCell,
+                    input_keep_prob=1.0,
+                    output_keep_prob=self.args.dropout
+                )
+            return encoDecoCell
+        encoDecoCell = tf.contrib.rnn.MultiRNNCell(
+            [create_rnn_cell() for _ in range(self.args.numLayers)],
+        )
+
+        # Network input (placeholders)
+
+        with tf.name_scope('placeholder_encoder'):
+            self.encoderInputs  = [tf.placeholder(tf.int32,   [None, ]) for _ in range(self.args.maxLengthEnco)]  # Batch size * sequence length * input dim
+
+        with tf.name_scope('placeholder_decoder'):
+            self.decoderInputs  = [tf.placeholder(tf.int32,   [None, ], name='inputs') for _ in range(self.args.maxLengthDeco)]  # Same sentence length for input and output (Right ?)
+            self.decoderTargets = [tf.placeholder(tf.int32,   [None, ], name='targets') for _ in range(self.args.maxLengthDeco)]
+            self.decoderWeights = [tf.placeholder(tf.float32, [None, ], name='weights') for _ in range(self.args.maxLengthDeco)]
+
+        # Define the network
+        # Here we use an embedding model, it takes integer as input and convert them into word vector for
+        # better word representation
+        decoderOutputs, states = tf.contrib.legacy_seq2seq.embedding_rnn_seq2seq(
+            self.encoderInputs,  # List<[batch=?, inputDim=1]>, list of size args.maxLength
+            self.decoderInputs,  # For training, we force the correct output (feed_previous=False)
+            encoDecoCell,
+            self.textData.getVocabularySize(),
+            self.textData.getVocabularySize(),  # Both encoder and decoder have the same number of class
+            embedding_size=self.args.embeddingSize,  # Dimension of each word
+            output_projection=outputProjection.getWeights() if outputProjection else None,
+            feed_previous=bool(self.args.test)  # When we test (self.args.test), we use previous output as next input (feed_previous)
+        )
+
+        # TODO: When the LSTM hidden size is too big, we should project the LSTM output into a smaller space (4086 => 2046): Should speed up
+        # training and reduce memory usage. Other solution, use sampling softmax
+
+        # For testing only
+        if self.args.test:
+            if not outputProjection:
+                self.outputs = decoderOutputs
+            else:
+                self.outputs = [outputProjection(output) for output in decoderOutputs]
+
+            # TODO: Attach a summary to visualize the output
+
+        # For training only
         else:
-            self.outputs, self.losses = tf.nn.seq2seq.model_with_buckets(
-                                        self.encoder_inputs, 
-                                        self.decoder_inputs, 
-                                        self.targets,
-                                        self.decoder_masks,
-                                        config.BUCKETS,
-                                        lambda x, y: _seq2seq_f(x, y, False),
-                                        softmax_loss_function=self.softmax_loss_function)
-        print('Time:', time.time() - start)
+            # Finally, we define the loss function
+            self.lossFct = tf.contrib.legacy_seq2seq.sequence_loss(
+                decoderOutputs,
+                self.decoderTargets,
+                self.decoderWeights,
+                self.textData.getVocabularySize(),
+                softmax_loss_function= sampledSoftmax if outputProjection else None  # If None, use default SoftMax
+            )
+            tf.summary.scalar('loss', self.lossFct)  # Keep track of the cost
 
-    def _creat_optimizer(self):
-        print('Create optimizer... \nIt might take a couple of minutes depending on how many buckets you have.')
-        with tf.variable_scope('training') as scope:
-            self.global_step = tf.Variable(0, dtype=tf.int32, trainable=False, name='global_step')
+            # Initialize the optimizer
+            opt = tf.train.AdamOptimizer(
+                learning_rate=self.args.learningRate,
+                beta1=0.9,
+                beta2=0.999,
+                epsilon=1e-08
+            )
+            self.optOp = opt.minimize(self.lossFct)
 
-            if not self.fw_only:
-                self.optimizer = tf.train.GradientDescentOptimizer(config.LR)
-                trainables = tf.trainable_variables()
-                self.gradient_norms = []
-                self.train_ops = []
-                start = time.time()
-                for bucket in range(len(config.BUCKETS)):
-                    
-                    clipped_grads, norm = tf.clip_by_global_norm(tf.gradients(self.losses[bucket], 
-                                                                 trainables),
-                                                                 config.MAX_GRAD_NORM)
-                    self.gradient_norms.append(norm)
-                    self.train_ops.append(self.optimizer.apply_gradients(zip(clipped_grads, trainables), 
-                                                            global_step=self.global_step))
-                    print('Creating opt for bucket {} took {} seconds'.format(bucket, time.time() - start))
-                    start = time.time()
+    def step(self, batch):
+        """ Forward/training step operation.
+        Does not perform run on itself but just return the operators to do so. Those have then to be run
+        Args:
+            batch (Batch): Input data on testing mode, input and target on output mode
+        Return:
+            (ops), dict: A tuple of the (training, loss) operators or (outputs,) in testing mode with the associated feed dictionary
+        """
 
+        # Feed the dictionary
+        feedDict = {}
+        ops = None
 
-    def _create_summary(self):
-        
-        pass
+        if not self.args.test:  # Training
+            for i in range(self.args.maxLengthEnco):
+                feedDict[self.encoderInputs[i]]  = batch.encoderSeqs[i]
+            for i in range(self.args.maxLengthDeco):
+                feedDict[self.decoderInputs[i]]  = batch.decoderSeqs[i]
+                feedDict[self.decoderTargets[i]] = batch.targetSeqs[i]
+                feedDict[self.decoderWeights[i]] = batch.weights[i]
 
-    def build_graph(self):
-        self._create_placeholders()
-        self._inference()
-        self._create_loss()
-        self._creat_optimizer()
-        self._create_summary()
+            ops = (self.optOp, self.lossFct)
+        else:  # Testing (batchSize == 1)
+            for i in range(self.args.maxLengthEnco):
+                feedDict[self.encoderInputs[i]]  = batch.encoderSeqs[i]
+            feedDict[self.decoderInputs[0]]  = [self.textData.goToken]
+
+            ops = (self.outputs,)
+
+        # Return one pass operator
+        return ops, feedDict
