d0a5c4254751fd29857d2f6cce80a7876bfc3ee5
Riotpiaole <rlian072@uottawa.ca>
2017-10-21 01:06:29 -0400

    Add log test on chatbot

--- chatbot/model.py
+++ chatbot/model.py
@@ -1,6 +1,35 @@
 import tensorflow as tf 
+import types 
 import numpy as np 
 from six.moves import xrange
+
+#  additional optimization as loop function 
+#  matching on tensorflow 1.3.1
+def loop_function(tensor,num_iteration,count,word_vec,state):
+    # assert isinstance(tensor.__call__,types.FunctionType)
+    """
+    function loop_function
+        loop over  number of iteration on tensor since the tensor are not 
+        iterable
+
+        input:
+            Tensor: 2D LSTM Tensor that will be iterated
+            args: batch feed_dict to the tensor.__call__
+                default: 0 -> wordvec_emb[:,i,:]
+                         1 -> states of lstm
+    """
+    if(num_iteration==count-1):
+        return tensor(word_vec[:,num_iteration,:],state)
+    
+    print( num_iteration,count,word_vec,state)
+    # compute the upcoming LSTM_call
+    _,states = tensor(word_vec[:,num_iteration,:],state)
+    print ("New ouput and state are : ",_," ",states)
+    num_iteration=num_iteration+1
+    # The next following states
+    print("Updating new args: ",args)
+    loop_function(tensor,num_iteration,count,args)
+    
 
 class seq2seq_chatbot():
     # dimension / shape of the word vector and nums of word will be n-gram
@@ -34,8 +63,8 @@ class seq2seq_chatbot():
         with tf.device("/cpu:0"):
             self.Wemb = tf.Variable(tf.random_uniform([n_words, dim_hidden], -0.1, 0.1), name='Weight_embedd_bias')
         # Generate the lstm cell
-        self.lstm1 = tf.contrib.rnn.BasicLSTMCell(dim_hidden,state_is_tuple=True)
-        self.lstm2 = tf.contrib.rnn.BasicLSTMCell(dim_hidden, state_is_tuple=True)
+        self.lstm1 = tf.contrib.rnn.BasicLSTMCell(dim_hidden,state_is_tuple=False)
+        self.lstm2 = tf.contrib.rnn.BasicLSTMCell(dim_hidden, state_is_tuple=False)
         
         # generate randomizes words vectors 
         with tf.name_scope("encode_vector"):
@@ -106,8 +135,8 @@ class seq2seq_chatbot():
                 loss = loss + current_loss
 
                 # tensorboard adding value 
-                tf.scalar_summary("sequence_loss",loss)
-                tf.scalar_summary("sequence_entropies",entropies)
+                # tf.scalar_summary("sequence_loss",loss)
+                # tf.scalar_summary("sequence_entropies",entropies)
 
             with tf.variable_scope(tf.get_variable_scope(), reuse=False): 
                 # train_op = tf.train.RMSPropOptimizer(self.lr).minimize(loss)
@@ -120,9 +149,6 @@ class seq2seq_chatbot():
             }
         
         return train_op, loss, word_vectors, caption, caption_mask, inter_value
-    
-    def loop_function():
-        pass
 
     def build_generator(self):
         with tf.name_scope("Sequence_Generator"):
@@ -140,39 +166,38 @@ class seq2seq_chatbot():
 
             probs = []
             embeds = []
-
             for i in xrange(self.n_encode_lstm_step):
                 if i > 0:
                     tf.get_variable_scope().reuse_variables()
-                print (wordvec_emb[:,i,:])
-                # with tf.variable_scope("LSTM1"):
-                #     output1, state1 = self.lstm1(wordvec_emb[:, i, :], state1)
+                
+                with tf.variable_scope("LSTM1"):
+                    output1, state1 = self.lstm1(wordvec_emb[:, i, :], state1)
+                
+                with tf.variable_scope("LSTM2"):
+                    output2, state2 = self.lstm2(tf.concat([padding, output1], 1), state2)
 
-        #         with tf.variable_scope("LSTM2"):
-        #             output2, state2 = self.lstm2(tf.concat([padding, output1], 1), state2)
+            for i in xrange(0, self.n_decode_lstm_step):
+                tf.get_variable_scope().reuse_variables()
 
-        #     for i in range(0, self.n_decode_lstm_step):
-        #         tf.get_variable_scope().reuse_variables()
+                if i == 0:
+                    with tf.device('/cpu:0'):
+                        current_embed = tf.nn.embedding_lookup(self.Wemb, tf.ones([1], dtype=tf.int64))
 
-        #         if i == 0:
-        #             with tf.device('/cpu:0'):
-        #                 current_embed = tf.nn.embedding_lookup(self.Wemb, tf.ones([1], dtype=tf.int64))
+                with tf.variable_scope("LSTM1"):
+                    output1, state1 = self.lstm1(padding, state1)
 
-        #         with tf.variable_scope("LSTM1"):
-        #             output1, state1 = self.lstm1(padding, state1)
+                with tf.variable_scope("LSTM2"):
+                    output2, state2 = self.lstm2(tf.concat([current_embed, output1], 1), state2)
 
-        #         with tf.variable_scope("LSTM2"):
-        #             output2, state2 = self.lstm2(tf.concat([current_embed, output1], 1), state2)
+                logit_words = tf.nn.xw_plus_b(output2, self.embed_word_W, self.embed_word_b)
+                max_prob_index = tf.argmax(logit_words, 1)[0]
+                generated_words.append(max_prob_index)
+                probs.append(logit_words)
 
-        #         logit_words = tf.nn.xw_plus_b(output2, self.embed_word_W, self.embed_word_b)
-        #         max_prob_index = tf.argmax(logit_words, 1)[0]
-        #         generated_words.append(max_prob_index)
-        #         probs.append(logit_words)
+                with tf.device("/cpu:0"):
+                    current_embed = tf.nn.embedding_lookup(self.Wemb, max_prob_index)
+                    current_embed = tf.expand_dims(current_embed, 0)
 
-        #         with tf.device("/cpu:0"):
-        #             current_embed = tf.nn.embedding_lookup(self.Wemb, max_prob_index)
-        #             current_embed = tf.expand_dims(current_embed, 0)
+                embeds.append(current_embed)
 
-        #         embeds.append(current_embed)
-
-        # return word_vectors, generated_words, probs, embeds
+        return word_vectors, generated_words, probs, embeds
--- chatbot/test.py
+++ chatbot/test.py
@@ -33,7 +33,7 @@ n_decode_lstm_step = 22
 
 batch_size = 1
 
-def test(model=default_model_path):
+def test(model=default_model_path,args='-l'):
     testing_data = open(testing_data_path,'r').read().split("\n")
 
     word_vector = KeyedVectors.load_word2vec_format('./word_vector.bin', binary=True)
@@ -49,9 +49,84 @@ def test(model=default_model_path):
             n_encode_lstm_step=n_encode_lstm_step,
             n_decode_lstm_step=n_decode_lstm_step,
             bias_init_vector=bias_init_vector)
-    # word_vectors, caption_tf, probs, _ =
-    model.build_generator()
+    word_vectors, caption_tf, probs, _ =model.build_generator()
     
+    sess = tf.InteractiveSession()
+
+    saver  = tf.train.Saver()
+
+    try:
+        print ('\n Restore pre-trained model: ',model," \n")
+        saver.restore(sess,model)
+    except:
+        print ('\n Uses Default movie Language model: ',model)
+        saver.restore(sess,default_model_path)
+    if args == '-l':
+        # for Different params to testing 
+        # This is for logging test 
+        with open(output_path, 'w') as out:
+            generated_sentences = []
+            bleu_score_avg = [0., 0.]
+            for idx, question in enumerate(testing_data):
+                print('question =>', question)
+
+                question = [data_parser.refine(w) for w in question.lower().split()]
+                question = [word_vector[w] if w in word_vector else np.zeros(dim_wordvec) for w in question]
+                question.insert(0, np.random.normal(size=(dim_wordvec,))) # insert random normal at the first step
+
+                if len(question) > n_encode_lstm_step:
+                    question = question[:n_encode_lstm_step]
+                else:
+                    for _ in range(len(question), n_encode_lstm_step):
+                        question.append(np.zeros(dim_wordvec))
+
+                question = np.array([question]) # 1x22x300
+
+                generated_word_index, prob_logit = sess.run([caption_tf, probs], feed_dict={word_vectors: question})
+                
+                # remove <unk> to second high prob. word
+                for i in range(len(generated_word_index)):
+                    if generated_word_index[i] == 3:
+                        sort_prob_logit = sorted(prob_logit[i][0])
+                        # print('max val', sort_prob_logit[-1])
+                        # print('second max val', sort_prob_logit[-2])
+                        maxindex = np.where(prob_logit[i][0] == sort_prob_logit[-1])[0][0]
+                        secmaxindex = np.where(prob_logit[i][0] == sort_prob_logit[-2])[0][0]
+                        # print('max ind', maxindex, ixtoword[maxindex])
+                        # print('second max ind', secmaxindex, ixtoword[secmaxindex])
+                        generated_word_index[i] = secmaxindex
+
+                generated_words = []
+                for ind in generated_word_index:
+                    generated_words.append(ixtoword[ind])
+
+                # generate sentence
+                punctuation = np.argmax(np.array(generated_words) == '<eos>') + 1
+                generated_words = generated_words[:punctuation]
+                generated_sentence = ' '.join(generated_words)
+
+                # modify the output sentence 
+                generated_sentence = generated_sentence.replace('<bos> ', '')
+                generated_sentence = generated_sentence.replace(' <eos>', '')
+                generated_sentence = generated_sentence.replace('--', '')
+                generated_sentence = generated_sentence.split('  ')
+                for i in range(len(generated_sentence)):
+                    generated_sentence[i] = generated_sentence[i].strip()
+                    if len(generated_sentence[i]) > 1:
+                        generated_sentence[i] = generated_sentence[i][0].upper() + generated_sentence[i][1:] + '.'
+                    else:
+                        generated_sentence[i] = generated_sentence[i].upper()
+                generated_sentence = ' '.join(generated_sentence)
+                generated_sentence = generated_sentence.replace(' i ', ' I ')
+                generated_sentence = generated_sentence.replace("i'm", "I'm")
+                generated_sentence = generated_sentence.replace("i'd", "I'd")
+                generated_sentence = generated_sentence.replace("i'll", "I'll")
+                generated_sentence = generated_sentence.replace("i'v", "I'v")
+                generated_sentence = generated_sentence.replace(" - ", "")
+
+                print('generated_sentence =>', generated_sentence)
+                out.write(generated_sentence + '\n')
+       
 
 if __name__ == '__main__':
     if len(sys.argv) >1:
