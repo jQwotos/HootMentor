1b089959099d2aaf59a43e1158831a3e7d38e905
Riotpiaole <rlian072@uottawa.ca>
2017-10-21 20:09:55 -0400

    Add trianing script, ready for trainning

--- chatbot/config.py
+++ chatbot/config.py
@@ -1,9 +1,9 @@
 # path to training data
-training_data_path = 'data/conversations_lenmax22_formersents2_with_former'
+training_data_path = './data/conversations_lenmax22_formersents2_with_former'
 filters = "+++$+++"
 
 # path to all_words
-all_words_path = 'data/all_words.txt'
+all_words_path = './data/all_words.txt'
 
 # training parameters 
 CHECKPOINT = True
@@ -14,7 +14,7 @@ start_batch = 0
 batch_size = 25
 
 # for RL training
-training_type = 'normal' # 'normal' for seq2seq training, 'pg' for policy gradient
+training_type = 'pg' # 'normal' for seq2seq training, 'pg' for policy gradient
 reversed_model_path = 'Adam_encode22_decode22_reversed-maxlen22_lr0.0001_batch25_wordthres6'
 reversed_model_name = 'model-63'
 
@@ -30,3 +30,5 @@ reversed_WC_threshold = 6
 
 # dialog simulation turns
 MAX_TURNS = 10
+
+dull_set = ["I don't know what you're talking about.", "I don't know.", "You don't know.", "You know what I mean.", "I know what you mean.", "You know what I'm saying.", "You don't know anything."]
